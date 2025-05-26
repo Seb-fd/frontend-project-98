700b4ae3c6a8934a13b9f923691730a4b50395a5
Seb-fd <138029510+Seb-fd@users.noreply.github.com>
2025-05-26 12:09:23 -0500

    Configure ESLint with Airbnb base, fixed linting issues, and updated .eslintrc.yml

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlinesync from "readline-sync";
+import readlinesync from 'readline-sync';
 
-export const userGreeting = () => {
-  console.log("¡Bienvenido a Brain Games!");
-  const name = readlinesync.question("¿Cuál es tu nombre?");
+export default function userGreeting() {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlinesync.question('¿Cuál es tu nombre?');
   console.log(`¡Hola, ${name}!`);
-};
+}
